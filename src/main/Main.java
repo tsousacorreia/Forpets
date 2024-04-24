2b2f329e623163ba64ca5ad883c0392b1b412c11
tsousacorreia <tsousacorreia@outlook.com>
2024-04-23 22:15:02 -0300

    Atualização 23-04-2024

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import View.TelaInicial_GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new TelaInicial_GUI().setVisible(true);
+    }
+    
+}
